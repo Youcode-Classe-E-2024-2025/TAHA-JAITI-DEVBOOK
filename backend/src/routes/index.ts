21b784560d9af5f6e369005ea77b5fcebc63ca1b
taha jaiti <kyojin.rar@gmail.com>
2025-04-23 14:30:14 +0100

    FEAT: registered the auth routes into the main router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import bookRoutes from './api/book.routes';
+import authRoutes from './api/auth.routes';
 
 const router = Router();
 
+router.use('/auth', authRoutes);
 router.use('/books', bookRoutes);
 
 
