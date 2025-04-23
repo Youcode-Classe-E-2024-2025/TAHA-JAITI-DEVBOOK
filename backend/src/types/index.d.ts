8c148efa1d4bc5576fd9bed07d524deec90caa50
taha jaiti <kyojin.rar@gmail.com>
2025-04-23 14:30:26 +0100

    FIX: changed the type of user to use our user

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,9 +1,10 @@
 import { JwtPayload } from 'jsonwebtoken';
+import type { UserType } from '../models/User';
 
 declare global {
     namespace Express {
         interface Request {
-            user?: string | JwtPayload;
+            user?: UserType;
         }
     }
 }
