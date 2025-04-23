f5a5fe0ae6ee17e030c7a2ee626354995b303a5e
taha jaiti <kyojin.rar@gmail.com>
2025-04-23 14:29:23 +0100

    FEAT: defined the create method in the book controller and book service

--- backend/src/controllers/BookController.ts
+++ backend/src/controllers/BookController.ts
@@ -1,14 +1,35 @@
 import type { Request, Response } from "express";
 import BookService from "../services/BookService";
-
+import Storage from "../util/storage";
 
 class BookController {
 
-    static async index(req: Request, res: Response){
+    static async index(req: Request, res: Response) {
         const books = await BookService.all();
         res.status(200).json(books);
     }
 
+    static async store(req: Request, res: Response) {
+        try {
+            const { fields, files } = await Storage.parse(req);
+
+            const bookData = {
+                title: fields.title?.[0] ?? '',
+                author: fields.auth?.[0] ?? '',
+                description: fields.description?.[0] ?? '',
+                cover_path: files.cover?.[0]?.filepath ?? '',
+                pdf_path: files.pdf?.[0]?.filepath ?? '',
+                user_id: req.user?.id!
+            };
+
+            const book = await BookService.create(bookData);
+
+            res.status(201).json(book);
+        } catch (err: unknown) {
+            res.status(400).json({ error: err });
+        }
+
+    }
 }
 
 export default BookController;
--- backend/src/services/BookService.ts
+++ backend/src/services/BookService.ts
@@ -1,11 +1,34 @@
-import Book from "../models/Book";
+import Book, { type BookType } from "../models/Book";
+import { validateBookData } from "../util/validator";
+import { CreateBookSchema, type CreateBookInput } from "../util/validator/book.validator";
 
 const model = new Book();
 
 class BookService {
 
-    static async all(){
+    static async all() {
         return await model.all();
+    }
+
+    static async create(data: CreateBookInput) {
+        const parsed = CreateBookSchema.safeParse(data);
+
+        if (!parsed.success) {
+            throw parsed.error;
+        }
+
+        const { title, author, description, cover_path, pdf_path, user_id } = parsed.data;
+
+        const bookData: Partial<BookType> = {
+            title,
+            author,
+            cover_path,
+            pdf_path,
+            user_id,
+        };
+
+        const book = await model.create(bookData);
+        return book;
     }
 
 }
