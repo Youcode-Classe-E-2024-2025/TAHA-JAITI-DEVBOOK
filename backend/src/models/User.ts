baa2a1f6a8b782d5d2de5b61cb2642fdf3d5ba57
taha jaiti <kyojin.rar@gmail.com>
2025-04-25 11:17:44 +0100

    FEAT: refactor User model interface to simplify structure and update types

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,29 +1,8 @@
-import type { RowDataPacket } from "mysql2";
-import db from "../db";
-import Model from "./Model";
-
-export interface UserType {
+export interface User {
     id?: number;
-    username: string;
+    name: string;
     email: string;
     password: string;
-    created_at?: string;
-    updated_at?: string;
-
+    created_at?: Date;
+    updated_at?: Date;
 }
-
-
-class User extends Model<UserType> {
-
-    constructor() {
-        super('users');
-    }
-
-    async findByMail(email: string): Promise<UserType | null> {
-        const [rows] = await db.query<RowDataPacket[]>(`SELECT * FROM users WHERE email = ?`, email);
-        return rows[0] as UserType || null;
-    }
-
-}
-
-export default User;
