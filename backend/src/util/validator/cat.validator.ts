ca19cd51bf2bca15d769e7ccfc2a1ef7c39dd3dd
taha jaiti <kyojin.rar@gmail.com>
2025-04-25 11:17:03 +0100

    REF: remove unused database, authentication, and validation files

--- backend/src/util/validator/cat.validator.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const CreateCategorySchema = z.object({
-    name: z.string().min(1, "name is required"),
-    user_id: z.number().int().positive("User ID should be a valid number")
-});
-
-export type CreateCatInput = z.infer<typeof CreateCategorySchema>;
