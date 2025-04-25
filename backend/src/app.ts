98e8b4e4712b09dcb4c8a9ae039f6e16aac82095
taha jaiti <kyojin.rar@gmail.com>
2025-04-25 11:17:33 +0100

    FEAT: restructure app initialization to include specific route imports and remove generic router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,26 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes';
+import { config } from './utils/config.js';
+import bookRoutes from './routes/bookRoutes';
+import categoryRoutes from './routes/categoryRoutes';
+import borrowingRoutes from './routes/borrowingRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', router);
+
+app.use('/api/books', bookRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/borrowings', borrowingRoutes);
+app.use('/api/auth', authRoutes);
+
+app.get('/', (_, res) => {
+    res.send('Welcome to the DevBook API!');
+});
 
 
 export default app;
